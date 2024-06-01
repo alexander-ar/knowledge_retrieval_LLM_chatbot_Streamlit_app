256e357a3148419450b57930974f31a00d89b8ba
alexander-ar <arefolov@gmail.com>
2024-06-01 17:04:33 -0400

    update requirements.txt start app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+# Install all libraries by running in the terminal: pip install -r requirements.txt
+import streamlit as st
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
